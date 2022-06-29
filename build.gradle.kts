5c1e7122242a7b89fda2acdc423a2c24b71d6415
Hyunsik Kang <kaka@linecorp.com>
2022-06-30 07:18:36 +0900

    versions up

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.6.7"
+    id("org.springframework.boot") version "2.7.1"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.6.21"
-    kotlin("plugin.spring") version "1.6.21"
-    kotlin("plugin.jpa") version "1.6.21"
+    kotlin("jvm") version "1.7.0"
+    kotlin("plugin.spring") version "1.7.0"
+    kotlin("plugin.jpa") version "1.7.0"
 }
 
 group = "com.example"
@@ -26,11 +26,11 @@ dependencies {
 
     // just add these dependencies for use kotlin-jdsl
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    val jdslVersion = "2.0.2.RELEASE"
+    val jdslVersion = "2.0.3.RELEASE"
     implementation("com.linecorp.kotlin-jdsl:spring-data-kotlin-jdsl-starter:$jdslVersion")
 
     // coroutine
-    val coroutineVersion = "1.6.1"
+    val coroutineVersion = "1.6.3"
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:$coroutineVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:$coroutineVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutineVersion")
@@ -38,12 +38,12 @@ dependencies {
 
     // reactive
     implementation("com.linecorp.kotlin-jdsl:spring-data-kotlin-jdsl-hibernate-reactive:$jdslVersion")
-    implementation("org.hibernate.reactive:hibernate-reactive-core:1.1.4.Final")
-    implementation("io.smallrye.reactive:mutiny-kotlin:1.4.0")
+    implementation("org.hibernate.reactive:hibernate-reactive-core:1.1.6.Final")
+    implementation("io.smallrye.reactive:mutiny-kotlin:1.6.0")
 
     // h2 db reactive
-    implementation("io.agroal:agroal-pool:1.15")
-    implementation("io.vertx:vertx-jdbc-client:4.2.7")
+    implementation("io.agroal:agroal-pool:2.0")
+    implementation("io.vertx:vertx-jdbc-client:4.3.1")
 }
 
 tasks.withType<KotlinCompile> {
